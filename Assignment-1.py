d0c6530202510852154a87d93680671beca3e70f
Jagrit <rjagrit.2k@gmail.com>
2026-05-16 16:21:00 +0530

    As-1

---
+++ Assignment-1.py
@@ -0,0 +1,10 @@
+# Assignment- 1 : Print Your name 10 times using Asterisk(*) symbol
+
+# Solution 
+
+# Method 1: Printing without Space
+# print("Jagrit Rattan" * 10)
+
+# Method 2: Printing name but each time it should be in new line/ next line
+
+print("Jagrit Rattan\n" * 10)
